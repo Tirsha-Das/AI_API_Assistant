a4462bf0d59764e788c09384f059358a19069097
Tirsha-Das <tish.das58@gmail.com>
2026-09-12 16:49:22 +0530

    gitignore updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,9 +3,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/healthcheck")
+@app.get("/health")
 def read_root():
-    return {"Hello World"}
+    return {"status": "ok"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
